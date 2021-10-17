2fd24c119ef06e134b39fd4228a13676d2dd7598
Éber Peretto <eberperetto@gmail.com>
2021-10-17 18:29:29 -0300

    Base API View, validating parameters

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,8 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("", views.RandomTypeless.as_view(), name="default"),
-    path("random", views.RandomTypeless.as_view(), name="random"),
-    path("random/<str:fact_type>", views.RandomTyped.as_view(), name="random_typed"),
-    path("<int:number>", views.NumberTypeless.as_view(), name="number"),
-    path("<int:month>/<int:day>", views.DateTyped.as_view(), name="date"),
-    path(
-        "<int:number>/<str:fact_type>", views.NumberTyped.as_view(), name="number_typed"
-    ),
+    path("", views.GetFact.as_view(), name="default"),
+    path("get_fact", views.GetFact.as_view(), name="get_fact"),
+    # TODO: get range of number facts using batch
+    # path("get_facts", views.GetFacts.as_view(), name="get_facts"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,95 +1,140 @@
-import json, requests
+import requests
+import datetime as dt
 from django.http.request import QueryDict
 from django.http.response import JsonResponse
 from django.views import View
 
-API_URL = "http://www.numbersapi.com"
+API_URL = "http://www.numbersapi.com/"
 
 
-def join_valid_get_arguments(get_arguments: QueryDict) -> str:
-    # Always appends json as a get argument
-    response = "?json"
-    if "fragment" in get_arguments:
-        response += "&fragment"
-    if "notfound" in get_arguments:
-        response += f"&notfound={get_arguments['notfound']}"
-    if "default" in get_arguments:
-        response += f"&default={get_arguments['default']}"
-    if "min" in get_arguments:
-        if "max" in get_arguments:
-            if get_arguments["min"] != "" and get_arguments["max"] != "":
-                response += f"&min={get_arguments['min']}&max={get_arguments['max']}"
+def get_validated_get_parameters(get_parameters: QueryDict) -> dict:
+    response = {
+        "number": "random",
+        "fact_type": None,
+        "date": None,
+        "error": None,
+    }
+
+    # Validate number
+    if "number" in get_parameters:
+        number = get_parameters["number"]
+        if validate_number(number) == False:
+            response[
+                "error"
+            ] = "Inform a valid number option: an integer or the word random"
+            return response
+        response["number"] = number
+
+    # Validate fact type
+    if "fact_type" in get_parameters:
+        fact_type = get_parameters["fact_type"]
+        if validate_fact_type(fact_type) == False:
+            response[
+                "error"
+            ] = "Invalid fact type. Should be one of: trivia, math, date or year"
+            return response
+        response["fact_type"] = fact_type
+
+    # Validate date request, which should have valid 'day' and 'month' parameters,
+    # paired with the "date" 'fact_type' parameter
+    if "month" in get_parameters:
+        if "day" in get_parameters:
+            month = get_parameters["month"]
+            if validate_month(month) == False:
+                response["error"] = "Invalid month. Should be a number from 1-12"
+                return response
+            day = get_parameters["day"]
+            if validate_day(day) == False:
+                response["error"] = "Invalid day. Should be a number from 1-31"
+                return response
+            date = f"{month}/{day}"
+            if validate_date(date) == False:
+                response["error"] = "Invalid date. Should be a valid month/day pair"
+                return response
+            if date is not None and response["fact_type"] != "date":
+                response[
+                    "error"
+                ] = "Invalid date/fact type pair. Should be a valid month/day paired with the fact_type 'date'"
+                return response
+            response["number"] = date
     return response
 
 
+def join_numbersapi_get_parameters(get_parameters: QueryDict) -> str:
+    # Always appends json as a get parameter
+    response = "?json"
+    if "fragment" in get_parameters:
+        response += "&fragment"
+    if "notfound" in get_parameters:
+        response += f"&notfound={get_parameters['notfound']}"
+    if "default" in get_parameters:
+        response += f"&default={get_parameters['default']}"
+    if "min" in get_parameters:
+        if "max" in get_parameters:
+            if get_parameters["min"] != "" and get_parameters["max"] != "":
+                response += f"&min={get_parameters['min']}&max={get_parameters['max']}"
+    return response
+
+
+def validate_number(number: str) -> bool:
+    if number == "random":
+        return True
+    try:
+        number = int(number)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_month(month: str) -> bool:
+    try:
+        month = int(month)
+        return month >= 1 and month <= 12
+    except ValueError:
+        return False
+
+
+def validate_day(day: str) -> bool:
+    try:
+        day = int(day)
+        return day >= 1 and day <= 31
+    except ValueError:
+        return False
+
+
+def validate_date(date: str) -> bool:
+    try:
+        # Validate date value using a leap year to ensure Feb. 29th validation
+        date = dt.datetime.strptime(f"{date}/2000", "%m/%d/%Y")
+        return True
+    except ValueError:
+        return False
+
+
 def validate_fact_type(fact_type: str) -> bool:
-    return fact_type in ["trivia", "math", "year"]
+    return fact_type in ["trivia", "math", "date", "year"]
 
 
-class RandomTypeless(View):
+def join_numbersapi_get_url(get_parameters: dict) -> str:
+    response = get_parameters["number"]
+    if get_parameters["fact_type"] is not None:
+        response += f"/{get_parameters['fact_type']}"
+    return response
+
+
+class GetFact(View):
+    """ Main view, handles every request combining the possible get parameters """
+
     def get(self, request, *args, **kwargs):
-        joined_valid_get_arguments = join_valid_get_arguments(self.request.GET)
-        r = requests.get(f"{API_URL}/random{joined_valid_get_arguments}")
-        # TODO: format response
-        return JsonResponse(r.json())
-
-
-class RandomTyped(View):
-    def get(self, request, *args, **kwargs):
-        fact_type = self.kwargs["fact_type"]
-        if validate_fact_type(fact_type) is False:
+        get_parameters = self.request.GET
+        validated_get_parameters = get_validated_get_parameters(get_parameters)
+        if validated_get_parameters.get("error", None) is not None:
             # TODO: format response
-            return JsonResponse(
-                {
-                    "text": "",
-                    "number": "random",
-                    "type": fact_type,
-                    "error": "Invalid fact type. Should be one of: trivia, math or year",
-                }
-            )
-        joined_valid_get_arguments = join_valid_get_arguments(self.request.GET)
-        r = requests.get(f"{API_URL}/random/{fact_type}{joined_valid_get_arguments}")
-        # TODO: format response
-        return JsonResponse(r.json())
+            return JsonResponse(validated_get_parameters)
 
+        numbersapi_get_url = join_numbersapi_get_url(validated_get_parameters)
 
-class NumberTypeless(View):
-    def get(self, request, *args, **kwargs):
-        # TODO: validate number
-        number = self.kwargs["number"]
-        joined_valid_get_arguments = join_valid_get_arguments(self.request.GET)
-        r = requests.get(f"{API_URL}/{number}{joined_valid_get_arguments}")
-        # TODO: format response
-        return JsonResponse(r.json())
-
-
-class NumberTyped(View):
-    def get(self, request, *args, **kwargs):
-        # TODO: validate number
-        number = self.kwargs["number"]
-        fact_type = self.kwargs["fact_type"]
-        if validate_fact_type(fact_type) is False:
-            # TODO: format response
-            return JsonResponse(
-                {
-                    "text": "",
-                    "number": number,
-                    "type": fact_type,
-                    "error": "Invalid fact type. Should be one of: trivia, math or year",
-                }
-            )
-        joined_valid_get_arguments = join_valid_get_arguments(self.request.GET)
-        r = requests.get(f"{API_URL}/{number}/{fact_type}{joined_valid_get_arguments}")
-        # TODO: format response
-        return JsonResponse(r.json())
-
-
-class DateTyped(View):
-    def get(self, request, *args, **kwargs):
-        # TODO: validate month and day
-        month = self.kwargs["month"]
-        day = self.kwargs["day"]
-        joined_valid_get_arguments = join_valid_get_arguments(self.request.GET)
-        r = requests.get(f"{API_URL}/{month}/{day}/date/{joined_valid_get_arguments}")
+        numbersapi_get_parameters = join_numbersapi_get_parameters(self.request.GET)
+        r = requests.get(f"{API_URL}{numbersapi_get_url}{numbersapi_get_parameters}")
         # TODO: format response
         return JsonResponse(r.json())
